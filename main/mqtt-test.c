d164fdf55498a47281a67b13b5491ac66abcec0c
fransstrom <frans.herrstrom@gmail.com>
2026-04-23 14:21:44 +0200

    button with publish new topic

--- main/mqtt-test.c
+++ main/mqtt-test.c
@@ -1,21 +1,27 @@
+#include "driver/gpio.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_netif.h"
 #include "esp_timer.h"
 #include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/gpio_types.h"
 #include "mqtt_client.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
 #include "secrets.h"
+#include "soc/gpio_num.h"
 #include <esp_wifi.h>
 #include <stdio.h>
-
 #define TAG "simple_connect_example"
 #include "protocol_examples_common.h"
 
 static const char *MQTT_TAG = "MQTT_TEST";
 static int counter = 0;
 static esp_mqtt_client_handle_t mqtt_client;
+
+#define BUTTON_GPIO GPIO_NUM_10
 
 static void publish_counter(void) {
   char payload[32];
@@ -64,10 +70,43 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
   }
 }
 
-static void periodic_timer_callback(void *arg) {
+void periodic_timer_callback(void *arg) {
   if (mqtt_client != NULL) {
     publish_counter();
   }
+}
+
+static void publish_boom(void) {
+  char payload[255];
+  snprintf(payload, sizeof(payload), "%s",
+           "panic button pressed from my esp32c3 SUPERMINI");
+  int msg_id = esp_mqtt_client_publish(mqtt_client, "boom", payload, 0, 1, 0);
+  ESP_LOGI(TAG, "Published counter=%d, msg_id=%d", counter, msg_id);
+  counter++;
+}
+static void on_button_press(void *args) {
+  uint8_t last_level = 1;
+
+  while (1) {
+    uint8_t level = gpio_get_level(BUTTON_GPIO);
+
+    if (level == 0 && last_level == 1) {
+      ESP_LOGI("BUTTON", "Pressed!");
+      if (mqtt_client != NULL) {
+        publish_boom();
+      }
+    }
+
+    last_level = level;
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
+void init_button() {
+  gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLUP_ONLY);
+
+  xTaskCreate(on_button_press, "button_task", 2048, NULL, 5, NULL);
 }
 
 void app_main(void) {
@@ -89,12 +128,14 @@ void app_main(void) {
   esp_mqtt_client_register_event(mqtt_client, ESP_EVENT_ANY_ID,
                                  mqtt_event_handler, NULL);
   esp_mqtt_client_start(mqtt_client);
-
-  const esp_timer_create_args_t periodic_timer_args = {
-      .callback = &periodic_timer_callback, .name = "periodic"};
-
-  esp_timer_handle_t periodic_timer;
-  ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-  ESP_ERROR_CHECK(
-      esp_timer_start_periodic(periodic_timer, 5000000)); // 5 seconds
+  //
+  // const esp_timer_create_args_t periodic_timer_args = {
+  //     .callback = &periodic_timer_callback, .name = "periodic"};
+  //
+  // esp_timer_handle_t periodic_timer;
+  // ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+  // ESP_ERROR_CHECK(
+  //     esp_timer_start_periodic(periodic_timer, 5000000)); // 5 seconds
+  //
+  init_button();
 }
